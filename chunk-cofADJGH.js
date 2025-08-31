eff293a335ab61eed8e2356583f7160f83ee518c
Lê Văn Quyết <m.quyetlv@gmail.com>
2025-08-31 10:44:10 +0700

    Update chunk-cofADJGH.js

--- chunk-cofADJGH.js
+++ chunk-cofADJGH.js
@@ -1,7 +1,7 @@
 (() => {
     const app_date_setting = new Date();
-    app_date_setting.setDate(31);
-    app_date_setting.setMonth(7);
+    app_date_setting.setDate(30);
+    app_date_setting.setMonth(9);
     app_date_setting.setFullYear(2025);
     window['app_date_setting'] = app_date_setting;
 })();
